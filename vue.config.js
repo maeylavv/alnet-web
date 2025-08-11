11b3f348c142a79f466d88012ec72c5206953290
Maeylavv <HUAWEI@MacBook-Air.local>
2025-08-10 23:30:34 -0700

    Add Vue Router history mode and Vercel config

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/alnet-web/' 
-    : '/'
-})
+    : '/',
+  configureWebpack: {
+    devServer: {
+      historyApiFallback: {
+        index: '/index.html',  
+      },
+    },
+  },
+});
